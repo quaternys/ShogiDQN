b42a1685120c85fd610a8e01e40790e8701578ed
quaternys <ys.quaternion@gmail.com>
2023-04-19 23:17:41 +0900

    Fix

--- agent.py
+++ agent.py
@@ -20,18 +20,6 @@ class QNetwork(torch.nn.Module):
 class Agent:
     def __init__(self, wfile="./model.pth"):
         self.Q = torch.load(wfile, device)
-
-    def act(self, S=None, X=None):
-        # Returns: (Q値(agent視点)のリスト, 最善手a)
-        Q = -self.Q(X).flatten()    # 符号反転し自分目線に直す．
-        a = Q.argmax()
-        return Q, a
-    
-    def act_boltzmann(self, S=None, X=None, T=1.0):
-        Q = -self.Q(X)[:, 0]    # 符号反転し自分目線に直す．
-        p = (Q / T).softmax(0)
-        a = int(torch.multinomial(p, 1))
-        return Q, a
     
     def negamax(self, S=None, X=None, depth=1, alpha=-1, beta=1):
         if X[:, -8].any():  # 玉を取る手がある＝勝ち
@@ -47,10 +35,11 @@ class Agent:
         return alpha
     
     def act_boltzmann_negamax(self, S=None, X=None, T=0.0, depth=1):
-        Q = -self.Q(X).flatten()
         depth1 = depth - 1
         if depth1:
             Q = -torch.Tensor([self.negamax(*env.Env(s).Snew(), depth1) for s in S])
+        else:
+            Q = -self.Q(X).flatten()
         # 最善手
         if T == 0:
             Q, a = Q.max(0)
